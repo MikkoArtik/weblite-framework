f3d6e66e6fc68297d292310d050bb6430331247a
Vyacheslav Laguta <chechelofka@gmail.com>
2025-08-29 17:54:00 +0300

    SP-3098: Исправление формата док стрингов, добавлена именованная передача агрументов.

--- weblite_framework/logging/logger.py
+++ weblite_framework/logging/logger.py
@@ -7,17 +7,31 @@ from datetime import datetime
 from logging.handlers import QueueHandler, QueueListener
 from queue import Queue
 
-__all__ = ['JsonFormatter', 'get_logger', 'get_handler']
+__all__ = [
+    'JsonFormatter',
+    'get_logger',
+]
 
 
 class JsonFormatter(logging.Formatter):
     """Форматируем лог-сообщения в JSON формате с полями.
 
-    Поля: timestamp, level, source, message
+    Поля:
+        timestamp: Временная метка.
+        level: Уровень логирования.
+        source: Источник сообщения (модуль).
+        message: Текст сообщения.
     """
 
     def format(self: 'JsonFormatter', record: logging.LogRecord) -> str:
-        """Форматируем запись лога в JSON строку."""
+        """Форматируем запись лога в JSON строку.
+
+        Args:
+            record(logging.LogRecord): Запись лога для форматирования.
+
+        Returns:
+              str: JSON-строка с отформатированным логом.
+        """
         log_record = {
             'timestamp': datetime.fromtimestamp(record.created)
             .astimezone()
@@ -33,15 +47,21 @@ _loggers: dict[str, logging.Logger] = {}
 
 
 def get_logger(name: str) -> logging.Logger:
-    """Создаем и возвращаем logger.
+    """Создаем и возвращаем логгер.
 
     Логирование происходит в отдельном потоке через QueueHandler.
+
+    Args:
+        name (str): Имя логгера.
+
+    Returns:
+        logging.Logger: Настроенный логгер.
     """
     if name in _loggers:
         return _loggers[name]
 
-    logger: logging.Logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger: logging.Logger = logging.getLogger(name=name)
+    logger.setLevel(level=logging.INFO)
     logger.addHandler(get_handler())
 
     _loggers[name] = logger
