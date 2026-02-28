39327a24b2d522a52060180bba8854b9d84c5bfb
Alex Klimenko <alkil9399@mail.ru>
2026-02-28 11:55:57 +0300

    SP-3958: Правка тредов

--- weblite_framework/provider/base_provider.py
+++ weblite_framework/provider/base_provider.py
@@ -1,73 +1,69 @@
 """Модуль для получения персональных данных с помощью базового провайдера."""
 
+from enum import Enum
 from typing import Any
 
 import aiohttp
+from yarl import URL
 
 __all__ = [
     'BaseProvider',
 ]
 
 
+class HttpMethod(str, Enum):
+    """Класс HTTP-методов для выполнения запросов."""
+
+    GET = 'GET'
+    POST = 'POST'
+    DELETE = 'DELETE'
+
+
 class BaseProvider:
     """Родительский класс HTTP-провайдера для подключения к сервису."""
 
-    def __init__(
-        self,
-        session: aiohttp.ClientSession,
-    ) -> None:
+    def __init__(self, base_url: URL, timeout: float) -> None:
         """Инициализирует экземпляр класса BaseProvider.
 
         Args:
-            session: HTTP-сессия
+            base_url: url для подключения к сервису
+            timeout: timeout запроса
         """
-        self._session = session
+        self._base_url = base_url
+        self._timeout = timeout
 
-    async def get_data(
+    async def request(
         self,
-        url: str,
+        method: HttpMethod,
+        path: str,
         params: dict[str, Any] | None = None,
-    ) -> dict[str, Any]:
-        """Выполняет get - запрос.
-
-        Args:
-            url: адрес эндпоинта
-            params: параметры запроса
-        Returns:
-            dict[str, Any]: ответ сервиса при успешном запросе
-        """
-        async with self._session.get(url, params=params) as response:
-            response.raise_for_status()
-            return await response.json()  # type: ignore[no-any-return]
-
-    async def post_data(
-        self,
-        url: str,
         data: dict[str, Any] | None = None,
-    ) -> dict[str, Any]:
-        """Выполняет post - запрос.
+    ) -> dict[str, Any] | None:
+        """Формирует общий вид запроса.
 
         Args:
-            url: адрес эндпоинта
+            method: тип запроса
+            path: адрес эндпоинта
+            params: параметры запроса
             data: тело запроса
+
         Returns:
             dict[str, Any]: ответ сервиса при успешном запросе
         """
-        async with self._session.post(url, json=data) as response:
-            response.raise_for_status()
-            return await response.json()  # type: ignore[no-any-return]
-
-    async def delete_data(
-        self,
-        url: str,
-    ) -> dict[str, Any]:
-        """Выполняет delete - запрос.
-
-        Args:
-            url: адрес эндпоинта
-        Returns:
-            dict[str, Any]: ответ сервиса при успешном запросе
-        """
-        async with self._session.delete(url) as response:
-            response.raise_for_status()
-            return await response.json()  # type: ignore[no-any-return]
+        timeout = aiohttp.ClientTimeout(total=self._timeout)
+        try:
+            async with aiohttp.ClientSession(timeout=timeout) as session:
+                async with session.request(
+                    method,
+                    url=self._base_url / path,
+                    params=params,
+                    data=data,
+                ) as response:
+                    response.raise_for_status()
+                    return await response.json()  # type: ignore[no-any-return]
+        except (
+            aiohttp.ClientResponseError,
+            aiohttp.ServerTimeoutError,
+            aiohttp.ClientError,
+        ):
+            return None
