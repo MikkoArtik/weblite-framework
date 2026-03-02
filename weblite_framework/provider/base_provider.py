04683bbd99f105ff4cf1211fd36b8e8aa30dba23
Alex Klimenko <alkil9399@mail.ru>
2026-03-02 18:21:18 +0300

    SP-3958: Правка тредов

--- weblite_framework/provider/base_provider.py
+++ weblite_framework/provider/base_provider.py
@@ -1,11 +1,12 @@
 """Модуль для получения персональных данных с помощью базового провайдера."""
 
+from http import HTTPMethod
 from typing import Any
 
 import aiohttp
+from aiohttp import ClientSession, ClientTimeout
 from yarl import URL
 
-from weblite_framework.enums import HttpMethodEnum
 from weblite_framework.exceptions.auth import UnauthorizedException
 from weblite_framework.exceptions.base import BaseAppException
 
@@ -14,26 +15,28 @@ __all__ = [
 ]
 
 
-class BaseProvider:
+class BaseProvider(ClientSession):
     """Родительский класс HTTP-провайдера для подключения к сервису."""
 
-    def __init__(self, base_url: URL, timeout: float) -> None:
+    def __init__(self, __base_url: URL, __timeout: float) -> None:
         """Инициализирует экземпляр класса BaseProvider.
 
         Args:
-            base_url: url для подключения к сервису
-            timeout: timeout запроса
+            __base_url: url для подключения к сервису
+            __timeout: timeout запроса
         """
-        self.__base_url = base_url
-        self.__timeout = timeout
+        super().__init__(
+            base_url=__base_url,
+            timeout=ClientTimeout(total=__timeout),
+        )
 
-    async def request(
+    async def _create_request(
         self,
-        method: HttpMethodEnum,
+        method: HTTPMethod,
         path: str,
         params: dict[str, Any] | None = None,
         data: dict[str, Any] | None = None,
-        headers: dict[str, Any] | None = None,
+        headers: dict[str, str] | None = None,
     ) -> dict[str, Any]:
         """Формирует общий вид запроса.
 
@@ -42,26 +45,22 @@ class BaseProvider:
             path: адрес эндпоинта
             params: параметры запроса
             data: тело запроса
-            headers: валидационные параметры запроса
+            headers: необязательные заголовки запроса
 
         Returns:
             dict[str, Any]: ответ сервиса при успешном запросе
         """
-        timeout = aiohttp.ClientTimeout(total=self.__timeout)
         try:
-            async with aiohttp.ClientSession(timeout=timeout) as session:
-                async with session.request(
-                    method,
-                    url=self.__base_url / path,
-                    params=params,
-                    data=data,
-                    headers=headers,
-                ) as response:
-                    payload = await response.json()
-                    await self.check_response_status(
-                        response=response,
-                    )
-                    return payload if payload else {}
+            async with super().request(
+                method=method,
+                url=path,
+                params=params,
+                data=data,
+                headers=headers,
+            ) as response:
+                payload = await response.json()
+                await self.check_response_status(response=response)
+                return payload if payload else {}
         except (aiohttp.ClientError, aiohttp.ServerTimeoutError) as exc:
             raise BaseAppException(
                 status_code=503,
@@ -82,9 +81,9 @@ class BaseProvider:
             BaseAppException: если статус 5xx или другой HTTP статус
         """
         if response.status == 401:
-            payload = await response.json()
-            message = payload.get('message')
-            raise UnauthorizedException(detail=message)
+            raise UnauthorizedException(
+                detail=f'Сервис вернул HTTP {response.status} - авторизация не пройдена.',  # noqa: E501
+            )
 
         if response.status >= 500:
             raise BaseAppException(
